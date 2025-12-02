6499adabe7c89faf0515aeacc4430d2ed3518b77
pankaj0417 <pankaj0172004@gmail.com>
2025-12-02 17:52:55 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import route from "./src/routes/userRouter.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+
 dotenv.config();
 
 const app = express();
@@ -14,9 +15,13 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: [
+      "http://localhost:5173",
+      "https://oncox.onrender.com"
+    ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
@@ -25,5 +30,5 @@ app.use("/api/user", route);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`server is running at port ${port} `);
+  console.log(`server is running at port ${port}`);
 });
